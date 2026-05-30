2d21f91cceaa57b2aa97c835a3e06754b1c65818
Gabriel Taguti <gabrieltaguti@MacBook-Pro-de-Gabriel.local>
2026-05-30 10:33:20 -0300

    chore: tidy ESLint configs; prepare for fixes

--- eslint.config.cjs
+++
@@ -1,9 +0,0 @@
-const { FlatCompat } = require('@eslint/eslintrc');
-const compat = new FlatCompat({ baseDirectory: __dirname });
-
-module.exports = [
-  ...compat.extends('next/core-web-vitals'),
-  {
-    ignores: ['.next/', 'node_modules/'],
-  },
-];
